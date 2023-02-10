b5cd68b6ca1da6f6d3f3383c09835c69c878fc61
KoketsuRyuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-02-10 12:33:44 +0000

    [応用課題9C]

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -6,11 +6,11 @@ class EventMailer < ApplicationMailer
     @body = event[:body]
     
     @mail = EventMailer.new()
-      mail(
-        from: ENV['MAIL_ADDRESS'],
-        to:   member.email,
-        subject: 'New Event Notice!'
-      )
+    mail(
+      from: ENV['MAIL_ADDRESS'],
+      to:   member.email,
+      subject: 'New Event Notice!!'
+    )
   end
   
   def self.send_notifications_to_group(event)
